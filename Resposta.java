95d11ce586a3b8d9dc026efcfff60c7c3a955639
icarocruz-cloud <icaro.cruz@ucsal.edu.br>
2026-04-03 20:33:20 -0300

    refatoração Resposta.java (solid)
    Mantidas como modelos de dados
    Sem lógica de negócio (apenas getters/setters)

---
+++ Resposta.java
@@ -0,0 +1,32 @@
+package br.com.ucsal.olimpiadas;
+
+public class Resposta {
+
+    private long questaoId;
+    private char alternativaMarcada;
+    private boolean correta;
+
+    public long getQuestaoId() {
+        return questaoId;
+    }
+
+    public void setQuestaoId(long questaoId) {
+        this.questaoId = questaoId;
+    }
+
+    public char getAlternativaMarcada() {
+        return alternativaMarcada;
+    }
+
+    public void setAlternativaMarcada(char alternativaMarcada) {
+        this.alternativaMarcada = alternativaMarcada;
+    }
+
+    public boolean isCorreta() {
+        return correta;
+    }
+
+    public void setCorreta(boolean correta) {
+        this.correta = correta;
+    }
+}
